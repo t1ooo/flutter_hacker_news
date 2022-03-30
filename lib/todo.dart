e09976eb6e0af30d852574037cd763c8af8b8fa5
graibn <5966878+t1ooo@users.noreply.github.com>
2022-03-30 14:19:35 +0500

    update

--- lib/todo.dart
+++ lib/todo.dart
@@ -4,11 +4,7 @@
 // TODO: replace cache to in memory lru
 // TODO: MAYBE: load story, user titles
 // TODO: static analytics options
-// TODO: remove comments
+// TODO: remove code comments
 // TODO: remove cache?
 // TODO: Http client with retry and cache
-// TODO: InMemoryLruCache flush to file (use delay + last update)
-// TODO: html error boundary
-// TODO: StoryTile
-//  onclick: go to Story
-//  add external link button
+
