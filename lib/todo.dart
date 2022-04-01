0fd48c2b69a9acfe307d080d0a6ce0c29e311ce8
graibn <5966878+t1ooo@users.noreply.github.com>
2022-04-01 11:10:52 +0500

    update

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,7 +1,5 @@
-// TODO: MAYBE: jump to comments prev | next
 // TODO: replace cache
 // TODO: MAYBE: load story and user titles
 // TODO: static analytics options
-// TODO: remove code comments
-// TODO: search https://hn.algolia.com/api/v1/search?query=test
-// TODO: updates
+// TODO: implement search https://hn.algolia.com/api/v1/search?query=test
+// TODO: implement updates
