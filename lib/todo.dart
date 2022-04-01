7d2731c63434f7166a3a38bcfcfeecfdd4598c3b
graibn <5966878+t1ooo@users.noreply.github.com>
2022-04-01 14:18:04 +0500

    update

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,3 +1,4 @@
+// TODO: run on adnroid emulator/device
 // TODO: replace cache to PersistenceInMemoryLruCache
 // TODO: implement updates
 // TODO: implement search https://hn.algolia.com/api/v1/search?query=test
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,96 +5,131 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hacker_news_prototype/logger.dart';
 import 'package:flutter_hacker_news_prototype/src/app.dart';
-import 'package:flutter_hacker_news_prototype/src/hacker_news_api/cache.dart';
 import 'package:flutter_hacker_news_prototype/src/hacker_news_api/hacker_news_api.dart';
-import 'package:flutter_hacker_news_prototype/src/hacker_news_api/http_client.dart';
-import 'package:flutter_hacker_news_prototype/src/notifier/story_notifier.dart';
-import 'package:flutter_hacker_news_prototype/src/provider/provider.dart';
+import 'package:flutter_hacker_news_prototype/src/stories/stories_screen.dart';
+import 'package:flutter_hacker_news_prototype/src/story/story_screen.dart';
+import 'package:flutter_hacker_news_prototype/src/user/user_screen.dart';
+import 'package:flutter_hacker_news_prototype/src/user_activities/user_activities_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
 import 'package:provider/provider.dart';
 
 Future<Provider<HackerNewsApi>> testHackerNewsApiProvider() async {
-  final cache = EternalFileCache(
-    File(
-      '/home/graibn/GoogleDrive/dev/project/source/flutter_hacker_news_prototype/data/data.json',
-    ),
-  );
-  await cache.load();
-  final httpClient = HttpClientImpl(Client(), cache, Throttle());
-  final hackerNewsApi = HackerNewsApiImpl(httpClient);
-
-  return Provider.value(value: hackerNewsApi);
+  return Provider<HackerNewsApi>.value(value: FakeHackerNewsApi());
 }
 
-void main() {
-  testWidgets('app widget smoke test', (WidgetTester tester) async {
-    // // Build our app and trigger a frame.
-    // await tester.pumpWidget(MyApp());
+Widget withMaterial(Widget child) {
+  return MaterialApp(home: child);
+}
 
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
-
-    // WidgetsFlutterBinding.ensureInitialized();
-
+Future<void> widgetTester(String name, Widget child) async {
+  testWidgets(name, (WidgetTester tester) async {
     configureLogger(true);
-
-    // final _hackerNewsApiProvider = await hackerNewsApiProvider();
-
-    // final cache = EternalFileCache(
-    //   File(
-    //     '/home/graibn/GoogleDrive/dev/project/source/flutter_hacker_news_prototype/data/data.json',
-    //   ),
-    // );
-    // await cache.load();
-    // final httpClient = HttpClientImpl(Client(), cache, Throttle());
-    // final hackerNewsApi = HackerNewsApiImpl(httpClient);
-
-    // final hackerNewsApi = FakeHackerNewsApi();
-    // final _hackerNewsApiProvider = Provider<HackerNewsApi>.value(value: FakeHackerNewsApi());
+    final _hackerNewsApiProvider = await testHackerNewsApiProvider();
 
     await tester.pumpWidget(
       MultiProvider(
         providers: [
-          Provider<HackerNewsApi>.value(value: FakeHackerNewsApi()),
+          _hackerNewsApiProvider,
         ],
-        child: MyApp(),
+        child: child,
       ),
     );
-
-    // final httpClient = HttpClientImpl(Client(), NoCache(), Throttle());
-    // final hackerNewsApi = HackerNewsApiImpl(httpClient);
-    // await tester.pumpWidget(
-    //   MultiProvider(
-    //     providers: [
-    //       // Provider<HackerNewsApi>.value(value: hackerNewsApi),
-    //       _hackerNewsApiProvider,
-    //       ChangeNotifierProvider<StoryNotifier>(
-    //         create: (BuildContext context) => StoryNotifier(
-    //           context.read<HackerNewsApi>(),
-    //         ),
-    //       ),
-    //     ],
-    //     child: MyApp(),
-    //   ),
-    // );
-    print('done');
-    // await Future.delayed(Duration(seconds: 5));
-
-    // cache.dispose();
   });
+}
+
+Future<void> main() async {
+  await widgetTester('MyApp widget smoke test', MyApp());
+
+  // NOTE: throw exception(No MediaQuery widget ancestor found) without Material
+  await widgetTester(
+    'MyApp widget smoke test',
+    withMaterial(StoriesScreen()),
+  );
+  await widgetTester(
+    'MyApp widget smoke test',
+    withMaterial(StoryScreen(id: 0)),
+  );
+  await widgetTester(
+    'MyApp widget smoke test',
+    withMaterial(UserActivitiesScreen(name: 'test-user')),
+  );
+  await widgetTester(
+    'MyApp widget smoke test',
+    withMaterial(UserScreen(name: 'test-user')),
+  );
+
+  // testWidgets('MyApp widget smoke test', (WidgetTester tester) async {
+  //   configureLogger(true);
+  //   final _hackerNewsApiProvider = await testHackerNewsApiProvider();
+
+  //   await tester.pumpWidget(
+  //     MultiProvider(
+  //       providers: [
+  //         _hackerNewsApiProvider,
+  //       ],
+  //       child: MyApp(),
+  //     ),
+  //   );
+  // });
+
+  // testWidgets('StoriesScreen widget smoke test', (WidgetTester tester) async {
+  //   configureLogger(true);
+  //   final _hackerNewsApiProvider = await testHackerNewsApiProvider();
+
+  //   await tester.pumpWidget(
+  //     MultiProvider(
+  //       providers: [
+  //         _hackerNewsApiProvider,
+  //       ],
+  //       // NOTE: throw exception(No MediaQuery widget ancestor found) without Material
+  //       child: withMaterial(StoriesScreen()),
+  //     ),
+  //   );
+  // });
+
+  // testWidgets('StoryScreen widget smoke test', (WidgetTester tester) async {
+  //   configureLogger(true);
+  //   final _hackerNewsApiProvider = await testHackerNewsApiProvider();
+
+  //   await tester.pumpWidget(
+  //     MultiProvider(
+  //       providers: [
+  //         _hackerNewsApiProvider,
+  //       ],
+  //       child: withMaterial(StoryScreen(id: 0)),
+  //     ),
+  //   );
+  // });
+
+  // testWidgets('UserActivitiesScreen widget smoke test',
+  //     (WidgetTester tester) async {
+  //   configureLogger(true);
+  //   final _hackerNewsApiProvider = await testHackerNewsApiProvider();
+
+  //   await tester.pumpWidget(
+  //     MultiProvider(
+  //       providers: [
+  //         _hackerNewsApiProvider,
+  //       ],
+  //       child: withMaterial(UserActivitiesScreen(name: 'test-user')),
+  //     ),
+  //   );
+  // });
+
+  // testWidgets('UserScreen widget smoke test', (WidgetTester tester) async {
+  //   configureLogger(true);
+  //   final _hackerNewsApiProvider = await testHackerNewsApiProvider();
+
+  //   await tester.pumpWidget(
+  //     MultiProvider(
+  //       providers: [
+  //         _hackerNewsApiProvider,
+  //       ],
+  //       child: withMaterial(UserScreen(name: 'test-user')),
+  //     ),
+  //   );
+  // });
 }
