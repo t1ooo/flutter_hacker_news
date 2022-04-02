c7ab761d78f829ea92345b6c1e2b524c25e374f2
graibn <5966878+t1ooo@users.noreply.github.com>
2022-04-02 16:09:19 +0500

    update

--- test/item_notifier_test.dart
+++ test/item_notifier_test.dart
@@ -1,9 +1,61 @@
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hacker_news_prototype/src/notifier/item_notifier.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import 'fake_hacker_news_api.dart';
 import 'mock.dart';
+
+void testFireNotifyListeners<T extends ChangeNotifier>(
+  String name,
+  FutureOr<T> Function() createNotifier,
+  Future<void> Function(T, MockListenerFunction) fn, {
+  int called = 1,
+}) async {
+  test('call $name should fire notifyListeners', () async {
+    final notifier = await createNotifier();
+    final listener = MockListenerFunction();
+    notifier.addListener(listener);
+    await fn(notifier, listener);
+    verify(listener).called(called);
+  });
+}
+
+// Future<void> testFireNotifyListenersV2<T extends ChangeNotifier>(
+//   String name,
+//   FutureOr<T> Function() createNotifier,
+//   Future<void> Function(T, MockListenerFunction) fn, {
+//   int called = 1,
+// }) async {
+//   final notifier = await createNotifier();
+//   final listener = MockListenerFunction();
+//   notifier.addListener(listener);
+//   await fn(notifier, listener);
+//   verify(listener).called(called);
+// }
+
+Future<void> testCalled(
+  Future<void> Function(MockListenerFunction) fn, [
+  int called = 1,
+]) async {
+  final listener = MockListenerFunction();
+  await fn(listener);
+  verify(listener).called(called);
+}
+
+/* 
+testFireNotifyListeners(notifier, () => itemNotifier.loadItem(id))
+
+
+void testFireNotifyListeners<T extends ChangeNotifier>(T notifier) {
+  final listener = MockListenerFunction();
+  notifier.addListener(listener);
+  await fn(notifier, listener);
+}
+
+ */
 
 Future<void> main() async {
   group('ItemNotifier', () {
@@ -17,28 +69,67 @@ Future<void> main() async {
         ..addListener(notifyListenerCallback);
     });
 
-    test('should fire notifyListeners', () async {
-      verifyNever(() => notifyListenerCallback());
+    // test('should fire notifyListeners', () async {
+    //   verifyNever(notifyListenerCallback);
 
-      await itemNotifier.loadItem(id);
-      verify(() => notifyListenerCallback()).called(1);
+    //   await itemNotifier.loadItem(id);
+    //   verify(notifyListenerCallback).called(1);
+    // });
+    // testFireNotifyListeners<ItemNotifier>(
+    //   'loadItem',
+    //   () => ItemNotifier(FakeHackerNewsApi()),
+    //   (itemNotifier, _) => itemNotifier.loadItem(id),
+    // );
+    test('should fire notifyListeners after loadItem', () async {
+      await testCalled((listener) async {
+        itemNotifier.addListener(listener);
+        await itemNotifier.loadItem(id);
+      });
     });
 
-    test('should fire notifyListeners', () async {
-      verifyNever(() => notifyListenerCallback());
+    // test('should fire notifyListeners', () async {
+    //   verifyNever(notifyListenerCallback);
 
-      await itemNotifier.reloadItem(id);
-      verify(() => notifyListenerCallback()).called(1);
+    //   await itemNotifier.reloadItem(id);
+    //   verify(notifyListenerCallback).called(1);
+    // });
+    // testFireNotifyListeners<ItemNotifier>(
+    //   'reloadItem',
+    //   () => ItemNotifier(FakeHackerNewsApi()),
+    //   (itemNotifier, _) => itemNotifier.reloadItem(id),
+    // );
+    test('should fire notifyListeners after reloadItem', () async {
+      await testCalled((listener) async {
+        itemNotifier.addListener(listener);
+        await itemNotifier.reloadItem(id);
+      });
     });
 
-    test('should fire notifyListeners', () async {
-      await itemNotifier.loadItem(id);
-      reset(notifyListenerCallback);
+    // test('should fire notifyListeners', () async {
+    //   await itemNotifier.loadItem(id);
+    //   reset(notifyListenerCallback);
 
-      verifyNever(() => notifyListenerCallback());
+    //   verifyNever(notifyListenerCallback);
 
-      await itemNotifier.reloadItems(awaited: true);
-      verify(() => notifyListenerCallback()).called(1);
+    //   await itemNotifier.reloadItems(awaited: true);
+    //   verify(notifyListenerCallback).called(1);
+    // });
+    // testFireNotifyListeners<ItemNotifier>(
+    //   'reloadItems',
+    //   () async {
+    //     final n = ItemNotifier(FakeHackerNewsApi());
+    //     await n.loadItem(id);
+    //     return n;
+    //   },
+    //   (itemNotifier, _) => itemNotifier.reloadItems(awaited: true),
+    // );
+    test('should fire notifyListeners after reloadItems', () async {
+      await testCalled((listener) async {
+        await itemNotifier.loadItem(id);
+
+        itemNotifier.addListener(listener);
+        await itemNotifier.reloadItems(awaited: true);
+      });
     });
 
     test('item should be not exist by default', () async {
