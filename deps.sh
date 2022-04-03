96dcb9c30c2f1acb93ec019f5b68d6d88f0c0079
graibn <5966878+t1ooo@users.noreply.github.com>
2022-04-03 14:42:05 +0500

    update

--- deps.sh
+++ deps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+deps
+
 flutter pub add url_launcher
 flutter pub add http
 flutter pub add json_annotation
@@ -7,20 +9,22 @@ flutter pub add equatable
 flutter pub add logging
 flutter pub add flutter_cache_manager
 flutter pub add provider
-# flutter pub add flutter_html
-# flutter pub add flutter_widget_from_html
 flutter pub add retry
 flutter pub add clock
 flutter pub add shimmer
-# flutter pub add html_unescape
 flutter pub add quiver
 flutter pub add intl
 flutter pub add html
-flutter pub add mocktail
-flutter pub add test
 flutter pub add path_provider
+# flutter pub add package_info_plus
+# flutter pub add flutter_widget_from_html
+# flutter pub add flutter_html
+# flutter pub add html_unescape
 
 flutter pub add build_runner --dev
 flutter pub add json_serializable --dev
+flutter pub add file --dev
+flutter pub add mocktail --dev
+flutter pub add test --dev
 
 flutter pub get
